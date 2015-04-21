bfcc60bb2c916f02e27b662e5da0ecf255901793
arifulislamnayan <farazi.aarif@gmail.com>
2015-04-21 15:55:24 +0600

    view/users/login.blade.php file corrected

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -3,9 +3,9 @@
 @section('body')
 
 {!! Form::open(array('url' => 'login', 'method' => 'post')) !!}
-{!! Form::label('email','Email'!!)}}
+{!! Form::label('email','Email') !!}
 {!! Form::text('email', null,array('class' => 'form-control')) !!}
-{!! Form::label('password','Password')!!}
+{!! Form::label('password','Password') !!}
 {!! Form::password('password',array('class' => 'form-control')) !!}
 {!! Form::submit('Login', array('class' => 'btn btn-primary')) !!}
 {!! Form::close() !!}
